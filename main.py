61f2a74a60c608394a9f0b16934223f4b712f1bf
Aptul <aptul4ever@gmail.com>
2021-07-17 17:42:02 +0200

    gira il database, crea cartelle e mette foto

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-def get_photo_from_video(video):
+def get_photo_from_video(video, directory, photo_name):
     import cv2
+    import os
     cap = cv2.VideoCapture(video)
     total_frames = cap.get(7)
 
 
     cap.set(1, int(total_frames/2))
     ret, still = cap.read()
-    cv2.imwrite(f'./frame.jpg', still)
+    current_dir = os.getcwd()
+    if not os.path.isdir(os.getcwd() + f"/{directory}"):
+        os.mkdir(os.getcwd() + f"/{directory}")
+    if not os.path.isfile(os.getcwd() + f"/{directory}/{photo_name}"):
+        os.chdir(os.getcwd() + "/" + directory)
+        cv2.imwrite(f"{photo_name}", still)
+        print("non presente")
+    else:
+        print("presente")
+    os.chdir(current_dir)
 
-    return 'frame.jpg'
 
 def image_cropper(photo):
     from PIL import Image
@@ -27,7 +36,31 @@ def image_resizer(photo):
     img.thumbnail((400, 400))
     img.save(photo)
 
+def do_job(video, directory, photo_name):
+    get_photo_from_video(video=video, directory=directory, photo_name=photo_name)
+    image_cropper(photo=f'{directory}/{photo_name}')
+    image_resizer(photo=f'{directory}/{photo_name}')
+if name == '__main__':
+    import pymongo
 
-get_photo_from_video(video="https://server5.streamingaw.online/DDL/ANIME/MairimashitaIruma-kun2/MairimashitaIruma-kun2_Ep_14_SUB_ITA.mp4")
-image_cropper(photo='frame.jpg')
-image_resizer(photo='frame.jpg')
+    myclient = pymongo.MongoClient('mongodb://admin:password@192.168.1.222:27017')
+    mydb = myclient["animes"]
+    mytb = mydb["anime_tb"]
+
+    for anime in mytb.find():
+        for episodio in anime["episodi"]:
+            if "AnimeWorld Server" in episodio.keys():
+                try:
+                    video = episodio["AnimeWorld Server"]
+                    directory = anime["_id"].replace("/", ",|").replace(".", ",.")
+                    photo_name = f'{episodio["numero"]}.jpg'
+
+                    do_job(video=video, directory=directory, photo_name=photo_name)
+
+                except Exception as e:
+                    print("AnimeWorld Server link non funzionante")
+                    print(anime)
+                    print(episodio)
+                    print(e)
+            else:
+                print("AnimeWorld Server non presente")
