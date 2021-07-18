71a391decba9f61b75f8521563a80f27d1f87733
Aptul <aptul4ever@gmail.com>
2021-07-18 06:52:18 +0200

    Update main.py
    aggiunto multiprocessing, ma la scansione va eseguita più volte perchè a volte il server rifiuta la connessione

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 def get_photo_from_video(video, directory, photo_name):
     import cv2
     import os
-    cap = cv2.VideoCapture(video)
-    total_frames = cap.get(7)
 
-
-    cap.set(1, int(total_frames/2))
-    ret, still = cap.read()
     current_dir = os.getcwd()
     if not os.path.isdir(os.getcwd() + f"/{directory}"):
         os.mkdir(os.getcwd() + f"/{directory}")
     if not os.path.isfile(os.getcwd() + f"/{directory}/{photo_name}"):
-        os.chdir(os.getcwd() + "/" + directory)
-        cv2.imwrite(f"{photo_name}", still)
-        print("non presente")
-    else:
-        print("presente")
+        cap = cv2.VideoCapture(video)
+        if cap.isOpened():
+            total_frames = cap.get(7)
+
+            cap.set(1, int(total_frames / 2))
+            ret, still = cap.read()
+            os.chdir(os.getcwd() + "/" + directory)
+            cv2.imwrite(f"{photo_name}", still)
+            print(f'written image {directory}/{photo_name}')
+        else:
+            print('non sono riuscito ad aprire il file')
+            print(video)
     os.chdir(current_dir)
 
 
@@ -36,31 +38,35 @@ def image_resizer(photo):
     img.thumbnail((400, 400))
     img.save(photo)
 
-def do_job(video, directory, photo_name):
-    get_photo_from_video(video=video, directory=directory, photo_name=photo_name)
-    image_cropper(photo=f'{directory}/{photo_name}')
-    image_resizer(photo=f'{directory}/{photo_name}')
+def do_job(anime):
+    for episodio in anime["episodi"]:
+        if "AnimeWorld Server" in episodio.keys():
+            try:
+                video = episodio["AnimeWorld Server"]
+                directory = anime["_id"].replace("/", ",|").replace(".", ",.")
+                photo_name = f'{episodio["numero"]}.jpg'
+
+                get_photo_from_video(video=video, directory=directory, photo_name=photo_name)
+                image_cropper(photo=f'{directory}/{photo_name}')
+                image_resizer(photo=f'{directory}/{photo_name}')
+
+            except Exception as e:
+                print("AnimeWorld Server link non funzionante")
+                print(anime)
+                print(episodio)
+                print(e)
+        else:
+            print("AnimeWorld Server non presente")
+
 if __name__ == '__main__':
     import pymongo
+    import multiprocessing
 
-    myclient = pymongo.MongoClient('mongodb://admin:password@192.168.1.222:27017')
-    mydb = myclient["animes"]
+    myclient = pymongo.MongoClient("mongodb://admin:password@aruculu.ddns.net:27017", connect=False)
+    mydb = myclient["animefs"]
     mytb = mydb["anime_tb"]
 
-    for anime in mytb.find():
-        for episodio in anime["episodi"]:
-            if "AnimeWorld Server" in episodio.keys():
-                try:
-                    video = episodio["AnimeWorld Server"]
-                    directory = anime["_id"].replace("/", ",|").replace(".", ",.")
-                    photo_name = f'{episodio["numero"]}.jpg'
+    animes = mytb.find()
+    p = multiprocessing.Pool(30)
+    p.map(do_job, animes)
 
-                    do_job(video=video, directory=directory, photo_name=photo_name)
-
-                except Exception as e:
-                    print("AnimeWorld Server link non funzionante")
-                    print(anime)
-                    print(episodio)
-                    print(e)
-            else:
-                print("AnimeWorld Server non presente")
